41b817db05cec79222bdbd929335a8f3127886b2
chenqingquan@jiweitech.com <chenqingquan@jiweitech.com>
2025-07-28 16:25:02 +0800

    test

---
+++ vue3-element-plus-project/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <el-button type="primary">这是一个Element Plus按钮</el-button>
+  </div>
+</template>
+
+<script setup>
+// 这里可以添加组件逻辑
+</script>
+
+<style scoped>
+/* 这里可以添加样式 */
+</style>
